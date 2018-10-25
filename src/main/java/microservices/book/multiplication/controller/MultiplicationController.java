e3ad8dabd99edfa2db85729a2c2b6bc4274d060e
Carvalho, Marcelo <Marcelo.Carvalho@mastercard.com>
2018-10-25 16:57:48 -0300

    Created MultiplicationController.java and MultiplicationControllerTest.java

---
+++ src/main/java/microservices/book/multiplication/controller/MultiplicationController.java
@@ -0,0 +1,27 @@
+package microservices.book.multiplication.controller;
+
+import microservices.book.multiplication.domain.Multiplication;
+import microservices.book.multiplication.service.MultiplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Marcelo Carvalho on 10/25/2018.
+ */
+@RestController
+@RequestMapping( "/multiplications" )
+public class MultiplicationController {
+    private final MultiplicationService multiplicationService;
+
+    @Autowired
+    public MultiplicationController( MultiplicationService multiplicationService ) {
+        this.multiplicationService = multiplicationService;
+    }
+
+    @GetMapping("/random")
+    Multiplication getRandomMultiplication() {
+        return multiplicationService.createRandomMultiplication();
+    }
+}
