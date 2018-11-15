a1e46481f9f974ec93fb07acd5d4e628af94c647
Marcelo Carvalho <mvalho@gmail.com>
2018-11-15 12:38:45 -0200

    Changed MultiplicationServiceImpl.java to user repositories

--- src/main/java/microservices/book/multiplication/service/MultiplicationServiceImpl.java
+++ src/main/java/microservices/book/multiplication/service/MultiplicationServiceImpl.java
@@ -2,9 +2,15 @@ package microservices.book.multiplication.service;
 
 import microservices.book.multiplication.domain.Multiplication;
 import microservices.book.multiplication.domain.MultiplicationResultAttempt;
+import microservices.book.multiplication.domain.User;
+import microservices.book.multiplication.repository.MultiplicationResultAttemptRepository;
+import microservices.book.multiplication.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
 
 /**
  * Created by Marcelo Carvalho on 10/25/2018.
@@ -12,10 +18,14 @@ import org.springframework.util.Assert;
 @Service
 public class MultiplicationServiceImpl implements MultiplicationService {
     private RandomGeneratorService randomGeneratorService;
+    private MultiplicationResultAttemptRepository attemptRepository;
+    private UserRepository userRepository;
 
     @Autowired
-    public MultiplicationServiceImpl( RandomGeneratorService randomGeneratorService ) {
+    public MultiplicationServiceImpl( final RandomGeneratorService randomGeneratorService, final MultiplicationResultAttemptRepository attemptRepository, final  UserRepository userRepository ) {
         this.randomGeneratorService = randomGeneratorService;
+        this.attemptRepository = attemptRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -26,13 +36,18 @@ public class MultiplicationServiceImpl implements MultiplicationService {
         return new Multiplication( factorA, factorB );
     }
 
+    @Transactional
     @Override
     public boolean checkAttempt( final MultiplicationResultAttempt resultAttempt ) {
-        boolean correct = resultAttempt.getResultAttempt() == resultAttempt.getMultiplication().getFactorA() * resultAttempt.getMultiplication().getFactorB();
+        Optional<User> user = userRepository.findByAlias(resultAttempt.getUser().getAlias());
         Assert.isTrue(!resultAttempt.isCorrect(), "You can't send an attempt marked as correct!!");
 
-        MultiplicationResultAttempt checkedAttempt = new MultiplicationResultAttempt(resultAttempt.getUser(), resultAttempt.getMultiplication(), resultAttempt.getResultAttempt(), correct);
+        boolean isCorrect = resultAttempt.getResultAttempt() == resultAttempt.getMultiplication().getFactorA() * resultAttempt.getMultiplication().getFactorB();
 
-        return correct;
+        MultiplicationResultAttempt checkedAttempt = new MultiplicationResultAttempt(user.orElse(resultAttempt.getUser()), resultAttempt.getMultiplication(), resultAttempt.getResultAttempt(), isCorrect);
+
+        attemptRepository.save(checkedAttempt);
+
+        return isCorrect;
     }
 }
--- src/test/java/microservices/book/multiplication/service/MultiplicationServiceImplTest.java
+++ src/test/java/microservices/book/multiplication/service/MultiplicationServiceImplTest.java
@@ -3,13 +3,19 @@ package microservices.book.multiplication.service;
 import microservices.book.multiplication.domain.Multiplication;
 import microservices.book.multiplication.domain.MultiplicationResultAttempt;
 import microservices.book.multiplication.domain.User;
+import microservices.book.multiplication.repository.MultiplicationResultAttemptRepository;
+import microservices.book.multiplication.repository.UserRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.verify;
 
 /**
  * Created by Marcelo Carvalho on 10/25/2018.
@@ -20,10 +26,16 @@ public class MultiplicationServiceImplTest {
     @Mock
     private RandomGeneratorService randomGeneratorService;
 
+    @Mock
+    private MultiplicationResultAttemptRepository attemptRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks( this );
-        multiplicationServiceImpl = new MultiplicationServiceImpl( randomGeneratorService );
+        multiplicationServiceImpl = new MultiplicationServiceImpl( randomGeneratorService, attemptRepository, userRepository );
     }
 
     @Test
@@ -43,12 +55,16 @@ public class MultiplicationServiceImplTest {
         Multiplication multiplication = new Multiplication( 50, 60 );
         User user = new User( "john_doe" );
         MultiplicationResultAttempt attempt = new MultiplicationResultAttempt( user, multiplication, 3000, false );
+        MultiplicationResultAttempt verifiedAttempt = new MultiplicationResultAttempt(user, multiplication, 3000, true);
+
+        given(userRepository.findByAlias("john_doe")).willReturn(Optional.empty());
 
         //when
         boolean attemptResult = multiplicationServiceImpl.checkAttempt( attempt );
 
         //then
         assertThat( attemptResult ).isTrue();
+        verify(attemptRepository).save(verifiedAttempt);
     }
 
     @Test
@@ -58,10 +74,13 @@ public class MultiplicationServiceImplTest {
         User user = new User( "john_doe" );
         MultiplicationResultAttempt attempt = new MultiplicationResultAttempt( user, multiplication, 3010, false );
 
+        given(userRepository.findByAlias("john_doe")).willReturn(Optional.empty());
+
         //when
         boolean attemptResult = multiplicationServiceImpl.checkAttempt( attempt );
 
         //then
         assertThat( attemptResult ).isFalse();
+        then(attemptRepository).should().save(attempt);
     }
 }
